05a3e465aded0bb1819b0e46b2643ff07fc2d657
Narek <133665009+Narek450@users.noreply.github.com>
2023-09-13 08:08:08 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'narekmanukyan450@gmail.com'
-EMAIL_HOST_PASSWORD = 'ntwbnikuqkxtpsec'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
